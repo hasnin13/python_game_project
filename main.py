a12a6c32fe7b52d82c08add002dfe5f715e8b57c
Hasnin Jahan Hanna <hasninjahanhannah@gmail.com>
2025-07-29 13:35:07 -0700

    .

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import pygame
+from settings import Settings
 
 class Main:
 
     def __init__(self):
         #pygame is a wrapper for SDL
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(size=(1200,800))
+        self.screen = pygame.display.set_mode(size=(
+            self.settings.width, 
+            self.settings.height
+            ))
 
+        pygame.display.set_caption('Alien Invention')
+
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont("Arial",24)
+        
+    
+    
     def run_game(self):
-        while True:
+         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     return
-                
-            pygame.display.flip()
 
+            self.screen.fill(self.settings.bg_color) 
+            self.render_fps(self.screen, self.clock, self.font)
+            #pygame.draw.rect(self.screen, (255,255,255),(100, 100,200,100))  
+
+            pygame.display.flip()
+            self.clock.tick()
+
+    def render_fps(self, screen,clock,font):
+        fps = int(clock.get_fps())
+        fps_text = font.render(f"FPS:{fps}", True, (0,0,0))
+        screen.blit(fps_text, (10,10))
 main = Main()
 main.run_game()
---
+++ settings.py
@@ -0,0 +1,5 @@
+class Settings:
+    def __init__(self):
+        self.height = 800
+        self.width = 1200
+        self.bg_color = (255,200,250)
