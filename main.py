93adf91c845cfee9d6cc8b92f8d9860ea330d62f
Hasnin Jahan Hanna <hasninjahanhannah@gmail.com>
2025-08-09 10:49:28 -0700

    .

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 from settings import Settings
 
+class Ship:
+    def __init__(self, screen):
+        ship_image = pygame.image.load('./images/spaceship.png')
+        self.ship_image = pygame.transform.scale(ship_image, (40,100))
+        self.screen = screen
+
+    def blitme(self):
+        self.screen.blit(self.ship_image,(400,100))
+
 class Main:
 
     def __init__(self):
@@ -15,14 +24,18 @@ class Main:
 
         pygame.display.set_caption('Alien Invention')
 
+        self.ship = Ship(self.screen)
+
         self.clock = pygame.time.Clock()
+        #font for fps
         self.font = pygame.font.SysFont("Arial",24)
         
     
     
     def run_game(self):
-         x = 100
-         y = 100
+         
+         x= 100
+         y= 100
          while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -31,9 +44,12 @@ class Main:
 
             self.screen.fill(self.settings.bg_color) # this is the bg
             self.render_fps(self.screen, self.clock, self.font)
+            self.ship.blitme()
+
+
             pygame.draw.rect(self.screen, (255,255,255),(x, y,200,100)) 
             x+= .1
-            y+= .2 
+            y+= .2
 
             pygame.display.flip()
             self.clock.tick()
@@ -42,5 +58,10 @@ class Main:
         fps = int(clock.get_fps())
         fps_text = font.render(f"FPS:{fps}", True, (0,0,0))
         screen.blit(fps_text, (10,10))
+
+
+
+
+
 main = Main()
 main.run_game()
